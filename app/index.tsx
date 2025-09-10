3ac1531c96ba761ab10c253bde986d2f6b4d7871
BLafleur <BDLAFLEUR21@gmail.com>
2025-09-10 10:43:51 -0600

    Edited index page
    -updated button styling
    -added logos to assets\images

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,66 @@
-import { Text, View } from "react-native";
+import { Pressable, Text, View, Image, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>SMPLE GYM APP WAHOOOOOOOO</Text>
+    <View style={styles.container}>
+      <Image 
+        source={require('../assets/images/Smple-removebg-preview.png')} 
+        style={styles.image}
+      />
+      <Text style={styles.welcomeText}>
+        Welcome to a more simple way to workout
+      </Text>
+
+      <Pressable style={styles.button}>
+        <Text style={styles.invertbuttonText}>Login</Text>
+      </Pressable>
+
+      <Pressable style={[styles.button, styles.signUpButton]}>
+        <Text style={styles.buttonText}>Sign Up</Text>
+      </Pressable>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+  },
+  image: {
+    marginBottom: 20,
+  },
+  welcomeText: {
+    marginBottom: 30,
+    fontSize: 16,
+    textAlign: "center",
+  },
+  button: {
+    backgroundColor: "#ffffffff", // iOS blue
+    paddingVertical: 12,
+    paddingHorizontal: 40,
+    borderRadius: 9999, // pill shape
+    marginVertical: 8,
+    shadowColor: "#000",
+    shadowOpacity: 0.2,
+    shadowRadius: 4,
+    shadowOffset: { width: 0, height: 2 },
+    minWidth: 200, // keeps pill shape consistent
+    alignItems: "center",
+  },
+  signUpButton: {
+    backgroundColor: "#000000ff", // iOS green
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "600",
+  },
+  invertbuttonText: {
+    color: "#000000ff",
+    fontSize: 16,
+    fontWeight: "600",
+  },
+});
